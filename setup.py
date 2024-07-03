01f07a324826b70b4f3a70712f47b81bc3282e81
Simon <103170889+simonbeguin@users.noreply.github.com>
2024-07-03 15:06:57 +0200

    Mise à jour vers la version 0.2.0

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
-
+import setuptools
 setup(
     name='Vinted_api_scraper',  # Nouveau nom de votre projet
-    version='0.3.0',
-    packages=find_packages(),
-    install_requires=[
-        # Listez ici les dépendances nécessaires, par exemple :
-        # 'requests',
-    ],
+    version='0.4.0',
     author='Simon BEGUIN',
     author_email='simon.beguin7@gmail.com',
-    description='Une description de votre bibliothèque',
+    description='Vinted API wrapper for python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/simonbeguin/Vinted_api_scraper',  # Lien vers votre dépôt GitHub ou autre
+    install_requires=["requests"],
+    keywords=["python", "Vinted api", "Vinted API wrapper", "python vinted"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
     ],
-    python_requires='>=3.6',
-)
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+    )
